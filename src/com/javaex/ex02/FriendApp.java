8e8e10c31aaddebdd65723ecd9b7b03ebad7eebb
imyoubin <imyoubin@hanmail.net>
2025-05-18 22:44:57 +0900

    2025.05.18

--- src/com/javaex/ex02/FriendApp.java
+++ src/com/javaex/ex02/FriendApp.java
@@ -1,13 +1,35 @@
 package com.javaex.ex02;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class FriendApp {
 
     public static void main(String[] args) {
 
-
+    	List<Friend> fList = new ArrayList<Friend>();
     	
+    	Scanner sc = new Scanner(System.in);
     	
+    	System.out.println("친구를 3명 등록해주세요");
+    	
+    	for(int i=0; i<3; i++){
+    		String line=sc.nextLine();
+    		String[] data = line.split(" ");
+    		
+    		Friend friend = new Friend();
+    		friend.setName(data[0]);
+    		friend.setHp(data[1]);
+    		friend.setSchool(data[2]);
+    		
+    		fList.add(friend);
+    		
+    	}
+    	for(int i=0; i<fList.size();i++) {
+    		fList.get(i).showInfo();
+    	}
+    	sc.close();    	
     }
 
 }
--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -1,9 +1,47 @@
 package com.javaex.ex03;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class GoodsApp {
 
     public static void main(String[] args) {
-
+    	
+    	List<Goods> gsList = new ArrayList<Goods>();
+        Scanner sc = new Scanner(System.in);
+        
+        int ttc = 0;
+        System.out.println("상품을 입력해주세요(종료 q)");
+        
+        while(true) {
+        	String line =sc.nextLine();
+        	if("q".equals(line)) {
+        		System.out.println("[입력완료]");
+        		System.out.println("=================");
+        		break;
+        		
+        	}
+        	String[] data = line.split(",");
+        	String na = data[0];
+        	int pr = Integer.parseInt(data[1]);
+        	int con = Integer.parseInt(data[2]);
+        	
+        	Goods goods = new Goods(na,pr,con);
+        	gsList.add(goods);
+        	
+        }
+        for(int i=0; i<gsList.size();i++) {
+        	gsList.get(i).showInfo();
+        	
+        }
+        for(int i=0;i<gsList.size();i++) {
+        	ttc=ttc+gsList.get(i).getCount();
+        	
+        }
+        System.out.println("모든 상품의 갯수는 "+ ttc+"개 입니다.");
+        sc.close();
+    	
        
     }
 
